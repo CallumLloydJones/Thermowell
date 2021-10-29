33c0a376d1e2d845734d412e3eecc7cb11c45cd7
Callum <jones.callumlloyd@gmail.com>
2021-10-29 15:44:03 +0100

    Frsh start

--- interpolation.py
+++
@@ -1,9 +0,0 @@
-from fluidfoam import readmesh
-import os
-
-cwd = "C:/Users/callu/OpenFoam-cases/Thermowell/"   # os.getcwd()
-
-coarse = cwd+'/coarse'
-fine = cwd+'/fine'
-
-x, y, z = readmesh(coarse)
